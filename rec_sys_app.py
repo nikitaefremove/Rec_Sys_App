b5f02aae4cf90331116c0e3e90ed53fc5dc6dfb5
Nikita <nikitaefremove@yandex.ru>
2023-08-15 20:52:04 +0700

    add load_model func, load_features func in rec_sys_app.py

--- rec_sys_app.py
+++ rec_sys_app.py
@@ -1,5 +1,10 @@
 from typing import List
 from fastapi import Depends, FastAPI, HTTPException
+import pandas as pd
+from sqlalchemy import create_engine
+import os
+from catboost import CatBoostClassifier
+import datetime
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import desc
 from sqlalchemy.sql.functions import count
@@ -8,16 +13,51 @@ from schema import UserGet, PostGet, FeedGet
 from table_user import User
 from table_post import Post
 from table_feed import Feed
+from schema import PostGet
 
 app = FastAPI()
 
-def get_db():
-    with SessionLocal() as db:
-        return db
 
-@app.get("/user/{id}", response_model=UserGet)
-def get_user_id(id: int, db: Session = Depends(get_db)) -> UserGet:
-    query_user_id = db.query(User).filter(User.id == id).one_or_none()
-    if not query_user_id:
-        raise HTTPException(404, "ID not found")
-    return query_user_id
+### Load catboost_model
+def get_model_path(path: str) -> str:
+    if os.environ.get("IS_LMS") == "1":  # Checking localhost or LMS service
+        MODEL_PATH = '/workdir/user_input/model'
+    else:
+        MODEL_PATH = path
+    return MODEL_PATH
+
+
+def load_models():
+    model_path = get_model_path("/Users/nikitaefremov/Documents/DATA_SCIENCE/SML_ML/REC_SYS/catboost_model")
+    model = CatBoostClassifier().load_model(model_path, format='cbm')
+    return model
+
+
+### Load features from database
+
+def batch_load_sql(query: str) -> pd.DataFrame:
+    CHUNKSIZE = 200000
+    engine = create_engine(
+        "postgresql://robot-startml-ro:pheiph0hahj1Vaif@"
+        "postgres.lab.karpov.courses:6432/startml"
+    )
+    conn = engine.connect().execution_options(stream_results=True)
+    chunks = []
+    for chunk_dataframe in pd.read_sql(query, conn, chunksize=CHUNKSIZE):
+        chunks.append(chunk_dataframe)
+    conn.close()
+    return pd.concat(chunks, ignore_index=True)
+
+
+def load_features() -> pd.DataFrame:
+    query = 'SELECT * FROM nikita_efremov_feature'
+    return batch_load_sql(query)
+
+
+### Endpoint
+@app.get("/post/recommendations/", response_model=List[PostGet])
+def recommended_posts(
+        id: int,
+        time: datetime,
+        limit: int = 10) -> List[PostGet]:
+    pass
