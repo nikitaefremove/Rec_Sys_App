963e8b4299e9d73be3ffce80f993b02d0e4994d4
Nikita <nikitaefremove@yandex.ru>
2023-08-16 16:22:11 +0700

    add predict func in rec_sys_app

--- rec_sys_app.py
+++ rec_sys_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 import os
 from catboost import CatBoostClassifier
-import datetime
+from datetime import datetime
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import desc
 from sqlalchemy.sql.functions import count
@@ -18,6 +18,12 @@ from schema import PostGet
 app = FastAPI()
 
 
+### Connection function
+def get_db():
+    with SessionLocal() as db:
+        return db
+
+
 ### Load catboost_model
 def get_model_path(path: str) -> str:
     if os.environ.get("IS_LMS") == "1":  # Checking localhost or LMS service
@@ -29,7 +35,7 @@ def get_model_path(path: str) -> str:
 
 def load_models():
     model_path = get_model_path("/Users/nikitaefremov/Documents/DATA_SCIENCE/SML_ML/REC_SYS/catboost_model")
-    model = CatBoostClassifier().load_model(model_path, format='cbm')
+    model = CatBoostClassifier().load_model('catboost_model', format='cbm')
     return model
 
 
@@ -49,21 +55,64 @@ def batch_load_sql(query: str) -> pd.DataFrame:
     return pd.concat(chunks, ignore_index=True)
 
 
-def load_features(user_id) -> pd.DataFrame:
-    query = f'SELECT * FROM nikita_efremov_feature WHERE user_id = {user_id}'
-    return batch_load_sql(query)
+def load_features() -> pd.DataFrame:
+    query1 = 'SELECT * FROM nikita_efremov_user_features_df'
+    query2 = 'SELECT * FROM nikita_efremov_post_features_df'
+    return batch_load_sql(query1), batch_load_sql(query2)
 
 
-### Endpoint
+### Function for prediction
+
+def prediction_top_5_posts(user_features_df, post_features_df, user_id, model):
+    ## Save the place for features is important for model
+    places_for_features_columns = ['user_id', 'post_id', 'gender', 'age', 'country', 'city',
+                                   'exp_group', 'os', 'source', 'count_actions', 'category_of_age',
+                                   'cluster_feature', 'month', 'day', 'second', 'weekday', 'is_weekend',
+                                   'feature_1', 'feature_2', 'feature_3', 'feature_4', 'feature_5',
+                                   'feature_6', 'feature_7', 'feature_8', 'feature_9', 'feature_10',
+                                   'feature_11', 'feature_12', 'feature_13', 'feature_14', 'feature_15',
+                                   'feature_16', 'feature_17', 'feature_18', 'feature_19', 'feature_20',
+                                   'feature_21', 'feature_22', 'feature_23', 'feature_24', 'feature_25',
+                                   'feature_26', 'feature_27', 'feature_28', 'feature_29', 'feature_30',
+                                   'feature_31', 'feature_32', 'feature_33', 'feature_34', 'feature_35',
+                                   'feature_36', 'feature_37', 'feature_38', 'feature_39', 'feature_40',
+                                   'feature_41', 'feature_42', 'feature_43', 'feature_44', 'feature_45',
+                                   'feature_46', 'feature_47', 'feature_48', 'feature_49', 'feature_50',
+                                   'part_of_day', 'topic']
+
+    # Create copy of dataframes and find the data of this user
+    this_user_data = user_features_df.copy().loc[user_features_df['user_id'] == user_id]
+    all_post_features_df = post_features_df.copy()
+
+    # Merge dataframes on key column
+    this_user_data['key'] = 1
+    all_post_features_df['key'] = 1
+    result = this_user_data.merge(all_post_features_df, on='key').drop('key', axis=1)
+    result = result[places_for_features_columns].set_index(['user_id', 'post_id'])
+    result['prediction'] = model.predict_proba(result)[:, 1]
+    top_5_posts = result.sort_values('prediction', ascending=False).head(5).index.get_level_values('post_id').tolist()
+    return top_5_posts
+
+
+### Endpoints
+
+### Get information about post by post_id
+@app.get("/post/{id}", response_model=PostGet)
+def get_post_id(id: int, db: Session = Depends(get_db)) -> PostGet:
+    query_post_id = db.query(Post).filter(Post.id == id).one_or_none()
+    if not query_post_id:
+        raise HTTPException(404, "ID not found")
+    return query_post_id
+
+
+### Get 5 recommendation of post to user
 @app.get("/post/recommendations/", response_model=List[PostGet])
 def recommended_posts(id: int, time: datetime, limit: int = 10) -> List[PostGet]:
     ### this endpoint has to return top 5 post_id, text
     pass
 
-df = load_features()
 
+df1, df2 = load_features()
 model = load_models()
 
-pred = model.predict_proba(df)
-
-print(pred)
+print(prediction_top_5_posts(df1, df2, 1800, model))
